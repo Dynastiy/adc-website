d15ca0c175e91cc88076545248df96bb82116b6b
Dynastiy <emmanuel00michael.com>
2021-12-22 16:33:16 +0100

    deploy

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
 <style>
   @import url("https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css");
   @import url("assets/css/style.css");
-  @import url("https://fonts.googleapis.com/css2?family=Work+Sans:wght@500&display=swap");
+  @import url("https://fonts.googleapis.com/css2?family=Work+Sans&display=swap");
   #app {
     font-family: Work Sans, sans-serif;
   }
