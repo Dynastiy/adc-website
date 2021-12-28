fb4c6a07e7c187adc3c34815a34eddc44fb23824
Dynastiy <emmanuel00michael.com>
2021-12-28 23:02:26 +0100

    fixed gallery

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -1,98 +1,53 @@
 <template>
-  <div>
-    <div class="container">
-      <div class="carousel-new">
-        <div class="carousel__face"></div>
-        <div class="carousel__face"></div>
-        <div class="carousel__face"></div>
-        <div class="carousel__face"></div>
-        <div class="carousel__face"></div>
-        <div class="carousel__face"></div>
-        <div class="carousel__face"></div>
-        <div class="carousel__face"></div>
-        <div class="carousel__face"></div>
+  <div class="content">
+    <div class="carousel-section py-5">
+        <client-only>
+          <carousel-3d
+            :perspective="0"
+            :controls-visible="true"
+            :autoplay="true"
+            :autoplay-timeout="3000"
+            :display="7"
+          >
+            <slide class="slide" :index="0">
+                <img src="@/assets/img/gallery/2.jpeg" width="100%" height="100%" class="slide_img" />
+            </slide>
+            <slide class="slide" :index="1">
+              <img src="@/assets/img/gallery/5.jpeg" width="100%" height="100%" class="slide_img" />
+            </slide>
+            <slide class="slide" :index="2">
+              <img src="@/assets/img/gallery/8.jpeg" width="100%" height="100%" class="slide_img" />
+            </slide>
+            <slide class="slide" :index="3">
+              <img src="@/assets/img/gallery/3.jpeg" width="100%" height="100%" class="slide_img" />
+            </slide>
+            <slide class="slide" :index="4">
+              <img src="@/assets/img/gallery/1.jpeg" width="100%" height="100%" class="slide_img" />
+            </slide>
+            <slide class="slide" :index="5">
+              <img src="@/assets/img/gallery/12.jpeg" width="100%" height="100%" class="slide_img" />
+            </slide>
+            <slide class="slide" :index="6">
+             <img src="@/assets/img/gallery/13.jpeg" width="100%" height="100%" class="slide_img" />
+            </slide>
+          </carousel-3d>
+        </client-only>
       </div>
-    </div>
   </div>
 </template>
 
-<style>
-  .container {
-    position: relative;
-    width: 280px;
-    margin: 100px auto 0 auto;
-    perspective: 1000px;
-  }
-
-  .carousel-new {
-    position: absolute;
-    bottom: 320px;
-    width: 100%;
-    height: 100%;
-    text-align: center;
-    transform-style: preserve-3d;
-    animation: rotate360 120s infinite forwards linear;
-  }
-  .carousel__face {
-    position: absolute;
-    width: 300px;
-    height: 187px;
-    top: 300px;
-    left: 10px;
-    right: 10px;
-    background-size: cover;
-    box-shadow: inset 0 0 0 2000px rgba(0, 0, 0, 0.5);
-    display: flex;
-  }
-
-  span {
-    margin: auto;
-    font-size: 2rem;
-  }
-
-  .carousel__face:nth-child(1) {
-    background-image: url("../assets/img/gallery/1.jpeg");
-    transform: rotateY(0deg) translateZ(430px);
-  }
-  .carousel__face:nth-child(2) {
-    background-image: url("../assets/img/gallery/2.jpeg");
-    transform: rotateY(40deg) translateZ(430px);
-  }
-  .carousel__face:nth-child(3) {
-    background-image: url("../assets/img/gallery/3.jpeg");
-    transform: rotateY(80deg) translateZ(430px);
-  }
-  .carousel__face:nth-child(4) {
-    background-image: url("../assets/img/gallery/4.jpeg");
-    transform: rotateY(120deg) translateZ(430px);
-  }
-  .carousel__face:nth-child(5) {
-    background-image: url("../assets/img/gallery/5.jpeg");
-    transform: rotateY(160deg) translateZ(430px);
-  }
-  .carousel__face:nth-child(6) {
-    background-image: url("../assets/img/gallery/6.jpeg");
-    transform: rotateY(200deg) translateZ(430px);
-  }
-  .carousel__face:nth-child(7) {
-    background-image: url("../assets/img/gallery/7.jpeg");
-    transform: rotateY(240deg) translateZ(430px);
-  }
-  .carousel__face:nth-child(8) {
-    background-image: url("../assets/img/gallery/8.jpeg");
-    transform: rotateY(280deg) translateZ(430px);
-  }
-  .carousel__face:nth-child(9) {
-    background-image: url("../assets/img/gallery/9.jpeg");
-    transform: rotateY(320deg) translateZ(430px);
-  }
-
-  @keyframes rotate360 {
-    from {
-      transform: rotateY(0deg);
+<script>
+export default {
+  
     }
-    to {
-      transform: rotateY(-360deg);
+    </script>
+
+    <style scoped>
+    .carousel-3d-slide .slide{
+      height: 100% !important;
+      width: 100% !important;
     }
-  }
-</style>
+    img{
+      object-fit: cover;
+    }
+    </style>
