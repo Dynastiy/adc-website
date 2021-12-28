577261f48373fa862d576b434a998adcff1d9e90
Dynastiy <emmanuel00michael.com>
2021-12-28 23:06:52 +0100

    obasanjo's message

--- src/router/index.js
+++ src/router/index.js
@@ -177,7 +177,7 @@ const routes = [{
                 // this generates a separate chunk (about.[hash].js) for this route
                 // which is lazy-loaded when the route is visited.
                 component: () =>
-                    import ( /* webpackChunkName: "about" */ '../pages/about/components/our_candidates.vue'),
+                    import ( /* webpackChunkName: "about" */ '../pages/about/components/obasanjos_message.vue'),
             },
             {
                 path: '/about/our_candidates',
