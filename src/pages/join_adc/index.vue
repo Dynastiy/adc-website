bfcdce5c78ba43161c1deb582af7a3e0b8378335
Dynastiy <emmanuel00michael.com>
2021-12-28 22:29:06 +0100

    fixed registration page

--- src/pages/join_adc/index.vue
+++ src/pages/join_adc/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <simplified-nav />
-    <div class="content">
+    <div class="content-tab">
       <h1 class="font-weight-bold text-darker mb-2">Join ADC</h1>
-      <p class="font-weight-bold text-main larger-text w-75 mb-5"> 
+      <p class="font-weight-bold text-darker larger-text w-75 mb-5"> 
         We’ve come a long way together. And our
         supporters made everything possible. <pre></pre> Membership is the best way to
         protect what we’ve achieved together and keep our progress going. You
@@ -12,23 +12,23 @@
       <h2 class="font-weight-bold mt-5 text-darker mb-5 text-center" >Choose a Membership Rate</h2>
      <div class="reg__cards">
           <div class="below_30 shadow p-3">
-        <h4 class="font-weight-bold text-center mt-2">Are you between 18 and 30?</h4>
-        <h5 class="text-center font-weight-bold"> &#8358;200</h5>
+        <!-- <h4 class="font-weight-bold text-center mt-2">Are you between 18 and 30?</h4> -->
+        <!-- <h5 class="text-center font-weight-bold"> &#8358;200</h5> -->
         <p class="font-weight-bold mt-5">
           Registration link for Persons between 18-30 years &
           Persons with Disabilities.
         </p>
         <router-link to="reg_form"
           ><button
-            class="bg-orange mt-3 w-100 btn font-weight-bold text-white small-btn-text"
+            class="bg-darker mt-3 w-100 btn font-weight-bold text-white small-btn-text"
           >
             Choose
           </button></router-link
         >
       </div>
       <div class="below_30 shadow p-3">
-        <h4 class="font-weight-bold text-darker text-center mt-2">Are you above 30?</h4>
-        <h5 class="text-center font-weight-bold"> &#8358; 500</h5>
+        <!-- <h4 class="font-weight-bold text-darker text-center mt-2">Are you above 30?</h4> -->
+        <!-- <h5 class="text-center font-weight-bold"> &#8358; 500</h5> -->
         <p class="font-weight-bold mt-5">
           Below is the Registration link for Persons above 30 years of age
         </p>
@@ -61,7 +61,7 @@
       </div>
       <div>
         <button
-          class="bg-main w-100 btn font-weight-bold text-white small-btn-text"
+          class="bg-darker w-100 btn font-weight-bold text-white small-btn-text"
         >
           I AM ALREADY A MEMBER, RENEW MY MEMBERSHIP
         </button>
@@ -81,9 +81,13 @@
 </script>
 
 <style>
+.content-tab {
+  padding: 2rem 8rem;
+}
 .below_30 {
-    background:#effff6;
+    /* background:#effff6; */
     border-radius: 5px;
+    width: 100%;
 }
 .content h1 {
     font-weight: 900;
